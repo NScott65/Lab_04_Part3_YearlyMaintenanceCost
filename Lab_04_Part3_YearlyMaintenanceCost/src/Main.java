ea1112ddd0effd57f54ec8d98ce7cb2605b3ea54
NScott65 <144457750+NScott65@users.noreply.github.com>
2023-09-19 13:50:35 -0400

    Add files via upload

--- Lab_04_Part3_YearlyMaintenanceCost/src/Main.java
+++ Lab_04_Part3_YearlyMaintenanceCost/src/Main.java
@@ -5,24 +5,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        //Variable
-        int initBal = 5000;
-        double apr;
-        double RATE = .17;
-        double month1;
-        double month2;
-        double month1Two;
-        double month2Two;
-        double apr2;
+        //Variables
+        double costSpring;
+        double costSummer;
+        double costFall;
+        double costWinter;
+        double totalCost;
 
-        apr = RATE / 12;
-        month1 = apr * initBal;
-        month1Two = initBal + month1;
-        System.out.println("Your balance after one month is: $" + month1Two + ".");
-        apr2 = RATE / 11;
-        month2 = month1Two * apr2;
-        month2Two = month2 + month1Two;
-        System.out.println("Your balance after two months is: $" + month2Two + ".");
+
+
+        System.out.println("Please enter maintenance for Spring: ");
+        costSpring = scan.nextDouble();
+        System.out.println("Please enter maintenance for Summer: ");
+        costSummer = scan.nextDouble();
+        System.out.println("Please enter maintenance for Fall: ");
+        costFall = scan.nextDouble();
+        System.out.println("Please enter maintenance for Winter: ");
+        costWinter = scan.nextDouble();
+        totalCost = costSpring + costSummer + costFall + costWinter;
+        System.out.println("Your total cost for the year is: $" + totalCost + ".");
 
     }
 }
